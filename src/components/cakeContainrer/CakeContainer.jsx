7bf6659b716687eac881ae7aafdeef311d5dd60d
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-05-02 17:31:12 +0330

    addde redux

---
+++ src/components/cakeContainrer/CakeContainer.jsx
@@ -0,0 +1,10 @@
+const CakeContainer = () => {
+  return (
+    <div>
+      <h2>Num of cakes : </h2>
+      <button type="button">Buy a cake</button>
+    </div>
+  );
+};
+
+export default CakeContainer;
---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,5 @@
+import { BUY_CAKE } from "./cakeTypes";
+
+export const buyCakeAction = () => {
+  return { type: BUY_CAKE };
+};
