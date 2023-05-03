5c4a285b1dc9d7857f93df87dd4938a27de678ed
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-05-03 16:19:27 +0330

    useSelector , useDispatch in react-redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 import store from "./redux/store";
 import CakeContainer from "./components/cakeContainrer/CakeContainer";
+import HookCakeContainer from "./components/HookCakeContainer/HookCakeContainer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="App">
         <h1>This is app</h1>
         <CakeContainer />
+        <HookCakeContainer />
       </div>
     </Provider>
   );
---
+++ src/components/HookCakeContainer/HookCakeContainer.jsx
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from "react-redux";
+import { buyCakeAction } from "../../redux/cake/cakeActions";
+
+const HookCakeContainer = () => {
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Num of cakes : {numOfCakes}</h2>
+      <button type="button" onClick={() => dispatch(buyCakeAction())}>
+        Buy a cake
+      </button>
+    </div>
+  );
+};
+
+export default HookCakeContainer;
